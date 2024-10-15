58484603e48277d1dac0f03f0900e04104519b7a
Leandrho <leandro.coder01@gmail.com>
2024-10-15 15:42:58 -0300

    add data news and pics

---
+++ src/data/news.js
@@ -0,0 +1,38 @@
+export const news = [
+    { 
+        id: 1,
+        title: "Fútbol Femenino",
+        brief: "¡Arrancó la temporada! La categoria sub 14 de femenino, ya comenzó su participación en la liga.",
+        description: "¡Arrancó la temporada! La categoria sub 14 de femenino, ya comenzó su participación en la liga. Se enfrentará con los equipos Eva Fútbol Club, San Luis FC, Fusión FC, entre otros. Proximo partido contra Fusión FC domingo 13 - 10:30hs Ave fénix.",
+        img: "/assets/images/noticias/arranca-femsbu14.jpeg",
+        priority: 1,
+        tags: []
+    },
+    { 
+        id: 2,
+        title: "Cronograma Domingo 13 y Lunes 14",
+        brief: "Este domingo y lunes Granaderos de Koslay juega de local",
+        description: "Este domingo y lunes Granaderos de Koslay juega de local. Todos juntos vamos a alentar a nuestros chicos!!",
+        img: "/assets/images/noticias/cronograma-13y14oct.jpeg",
+        priority: 2,
+        tags: []
+    },
+    { 
+        id: 3,
+        title: "Fecha Nro 7 - 7ma Divisíon",
+        brief: "",
+        description: "La 7ma este Domingo 6 de Octubre a las 17hs, se enfrenta a San Antonio E.G",
+        img: "/assets/images/noticias/fecha7-7ma.jpg",
+        priority: 2,
+        tags: []
+    },
+    { 
+        id: 4,
+        title: "Fecha Nro 7 - 9na Divisíon",
+        brief: "",
+        description: "La 9na este Domingo 6 de Octubre a las 10hs, se enfrenta a el Club Atletico El Bufalo",
+        img: "/assets/images/noticias/fecha7-9na.jpg",
+        priority: 2,
+        tags: []
+    },
+];
