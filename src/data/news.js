a974722ad03ec8abb082b8b6197935c4ee6b64ac
Leandrho <leandro.coder01@gmail.com>
2025-05-07 13:11:17 -0300

    add news and change img to arrayImg

--- src/data/news.js
+++ src/data/news.js
@@ -4,7 +4,7 @@ export const news = [
         title: "¡Se viene la 2ª fecha del Torneo Apertura! ⚽ ",
         brief: "Consulta el cronograma completo",
         description: "Este fin de semana, el club se viste de gala para recibir la segunda fecha del Torneo Apertura.\n¡Prepárate para vivir jornadas llenas de pasión y fútbol! \n¡No te pierdas ningún partido! Ven a disfrutar de un fin de semana a puro fútbol en familia.",
-        img: "/assets/images/noticias/crono-32025.jpeg",
+        img: ["/assets/images/noticias/crono-32025.jpeg"],
         date: "18 de Marzo del 2025",
         priority: 2,
         tags: []
@@ -14,7 +14,7 @@ export const news = [
         title: "En esta fecha visitamos a Serranías F.C ⚽ ",
         brief: "¡No te pierdas este partidazo! ¡Vení a alentar al club!",
         description: "En la segunda fecha del Torneo Apertura, nuestro equipo visitará a Serranías F.C. ¡Vamos a alentar al club!",
-        img: "/assets/images/noticias/vsserranias32025.jpeg",
+        img: ["/assets/images/noticias/vsserranias32025.jpeg"],
         date: "18 de Marzo del 2025",
         priority: 2,
         tags: []
@@ -24,7 +24,7 @@ export const news = [
         title: "¡Tu pasión, tu equipo! Sumate al femenino de Granaderos de Koslay",
         brief: "¡No te pierdas esta oportunidad! ¡Inscribite ya!",
         description: "¿Tenés entre 12 y 16 años y te apasiona el fútbol? ¡Esta es tu oportunidad!\nVení a formar parte del equipo femenino de Granadas y desarrollá todo tu talento.",
-        img: "/assets/images/noticias/femFree.jpeg",
+        img: ["/assets/images/noticias/femFree.jpeg"],
         date: "19 de Marzo del 2025",
         priority: 2,
         tags: []
@@ -34,7 +34,7 @@ export const news = [
         title: "La categoría 2015 dice presente en el Apertura",
         brief: "Semillero de campeones",
         description: "Nuestros pequeños gigantes de la categoría 2015 están dejando todo en la cancha. Con pasión y garra, estos futuros cracks están demostrando su talento en el Torneo Apertura 2025.\n \n¡Agradecemos a los padres, familiares y entrenadores por su apoyo incondicional!\n \n¡Vení a alentar a nuestros pequeños gigantes! ¡Te esperamos en la cancha!",
-        img: "/assets/images/noticias/team-15-32025.jpeg",
+        img: ["/assets/images/noticias/team-15-32025.jpeg"],
         date: "19 de Marzo del 2025",
         priority: 2,
         tags: []
@@ -44,7 +44,7 @@ export const news = [
         title: "Horarios de entrenamiento actualizados",
         brief: "¡Entrená con nosotros! Consultá los horarios de cada categoría",
         description: "En la imagen, encontrarás los días y horarios de entrenamiento.\n\n¡Te esperamos en la cancha para vivir la pasión del fútbol!",
-        img: "/assets/images/noticias/horarios-32025.jpeg",
+        img: ["/assets/images/noticias/horarios-32025.jpeg"],
         date: "19 de Marzo del 2025",
         priority: 2,
         tags: []
@@ -54,7 +54,7 @@ export const news = [
         title: "¡Sub 14 femenino!",
         brief: "Este domingo 27 de abril se vive un gran duelo en la cancha de Ave Fénix.",
         description: "Granaderos de Koslay se enfrentan a Búfalo de La Punta en un partido que promete emoción y garra desde el primer minuto.\n\nHora: 12:30 hrs\nLugar: Cancha Ave Fénix \n\n¡No faltes! Vení a apoyar a tu equipo y disfruta de una jornada llena de buen fútbol.",
-        img: "/assets/images/noticias/femenino27abril.jpeg",
+        img: ["/assets/images/noticias/femenino27abril.jpeg"],
         date: "22 de Abril del 2025",
         priority: 2,
         tags: []
@@ -64,9 +64,9 @@ export const news = [
         title: "Este domingo se vive el fútbol con todo!",
         brief: "Granaderos de Koslay vs EFI Junior\n Domingo 27 de abril  9:30 AM",
         description: "Cancha: Ave Fénix\n\nSe viene un partidazo y vos tenés que estar ahí.\n¡Vamos a alentar con todo! ¡No importa el frío, no importa la hora, lo importante es el aguante!",
-        img: "/assets/images/noticias/masc200827abril.jpeg",
+        img: ["/assets/images/noticias/masc200827abril.jpeg"],
         date: "22 de Abril del 2025",
-        priority: 1,
+        priority: 2,
         tags: []
     },
     { 
@@ -74,7 +74,7 @@ export const news = [
         title: "Los más peques también juegan con el corazón!",
         brief: "Categoría 2013\nGranaderos de Koslay vs serranías fc \nDomingo 27 de abril | 11:00 hrs\nCancha: Ave Fénix",
         description: "Nuestros mini cracks salen a la cancha a dejarlo todo.\n\n¡Vení a acompañarlos, alentá con fuerza y disfrutá del futuro del fútbol!\nNo faltes, que cada aplauso cuenta.",
-        img: "/assets/images/noticias/peque27abril.jpeg",
+        img: ["/assets/images/noticias/peque27abril.jpeg"],
         date: "22 de Abril del 2025",
         priority: 2,
         tags: []
@@ -84,9 +84,19 @@ export const news = [
         title: "¡VAMOS GRANADEROS DE KOSLAY! 2015 Presente con Orgullo",
         brief: "Este domingo los Granaderos de Koslay visitan al Club Lince",
         description: "Este domingo los Granaderos de Koslay visitan al Club Lince en busca de un triunfo contundente.\n\n¡A dejar el alma en la cancha!\n¡Vamos Granaderos!",
-        img: "/assets/images/noticias/masc201527abril.jpeg",
+        img: ["/assets/images/noticias/masc201527abril.jpeg"],
         date: "22 de Abril del 2025",
         priority: 2,
+        tags: []
+    },
+    { 
+        id: 14,
+        title: "Próximos partidos - Sabado 10 de Mayo",
+        brief: "Este sabado se vienen grandes duelos - Vamos Granaderos!!",
+        description: "Aca tenes el detalle de todos los partidos de las distintas categorias.\n\n¡A dejar el alma en la cancha!\n¡Vamos Granaderos!",
+        img: ["/assets/images/noticias/20250507-1.jpeg", "/assets/images/noticias/20250507-2.jpeg", "/assets/images/noticias/20250507-3.jpeg", "/assets/images/noticias/20250507-4.jpeg"],
+        date: "7 de Mayo del 2025",
+        priority: 1,
         tags: []
     },
 
