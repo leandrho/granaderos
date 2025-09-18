7d69d84f68381838e8c8b1724942a2f9edf8f237
Leandrho <leandro.coder01@gmail.com>
2025-09-18 15:29:08 -0300

    new

--- src/data/news.js
+++ src/data/news.js
@@ -627,6 +627,17 @@ export const news = [
         links: [],
         tags: []
     },
+    { 
+        id: 107,
+        title: "Categoria 2015 💪⚽",
+        brief: "",
+        description: "\n#VamosGranaderos #FútbolJuvenil #GranaderosEnAcción",
+        img: ["/assets/images/noticias/2025-09-18-07.jpeg"],
+        date: "18 de Septiembre del 2025",
+        priority: 2,
+        links: [],
+        tags: []
+    },
 
 
 
