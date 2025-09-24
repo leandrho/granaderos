a3315fdcf296c143d28559a25cb4fae0f4fe811a
Leandrho <leandro.coder01@gmail.com>
2025-09-24 16:55:22 -0300

    new

--- src/data/news.js
+++ src/data/news.js
@@ -660,6 +660,17 @@ export const news = [
         links: [],
         tags: []
     },
+    { 
+        id: 110,
+        title: "Categoria 2013 💪⚽",
+        brief: "",
+        description: "\n#VamosGranaderos #FútbolJuvenil #GranaderosEnAcción",
+        img: ["/assets/images/noticias/2025-09-24-03.jpeg"],
+        date: "24 de Septiembre del 2025",
+        priority: 2,
+        links: [],
+        tags: []
+    },
 
 
 
