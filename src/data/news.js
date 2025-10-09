77c9e8dd8b3a0ec73881c6807b3aa3b1ec93c56f
Leandrho <leandro.coder01@gmail.com>
2025-10-09 16:22:15 -0300

    add new

--- src/data/news.js
+++ src/data/news.js
@@ -605,6 +605,17 @@ export const news = [
         links: [],
         tags: []
     },
+    { 
+        id: 125,
+        title: "Categoria 2008 💪⚽",
+        brief: "",
+        description: "\n#VamosGranaderos #FútbolJuvenil #GranaderosEnAcción",
+        img: ["/assets/images/noticias/2025-10-09-04.jpeg"],
+        date: "09 de Octubre del 2025",
+        priority: 2,
+        links: [],
+        tags: []
+    },
 
 
 
