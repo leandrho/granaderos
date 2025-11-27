d1146f78d2765b1a78b46389a93fc72ae1761960
Leandrho <leandro.coder01@gmail.com>
2025-11-27 11:58:09 -0300

    new

--- src/data/news.js
+++ src/data/news.js
@@ -230,6 +230,17 @@ export const news = [
         links: [],
         tags: []
     },
+    { 
+        id: 164,
+        title: "Categoria Sub14 Femenino💪⚽",
+        brief: "",
+        description: "\n#VamosGranaderos #FútbolJuvenil #GranaderosEnAcción",
+        img: ["/assets/images/noticias/2025-11-26-03.jpeg"],
+        date: "26 de Noviembre del 2025",
+        priority: 2,
+        links: [],
+        tags: []
+    },
     
 
 
