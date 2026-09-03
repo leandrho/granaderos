4f0f9ffaccc925e58fb36c434e004d43bc7d37a2
Leandro <leandro.coder01@gmail.com>
2026-09-03 16:04:47 -0300

    add new

--- src/data/news.js
+++ src/data/news.js
@@ -274,6 +274,17 @@ export const news = [
         links: [],
         tags: []
     },
+    { 
+        id: 224,
+        title: "Categoria Sub10 ⚽⚽",
+        brief: "",
+        description: "\n#VamosGranaderos #FútbolJuvenil #GranaderosEnAcción",
+        img: ["/assets/images/noticias/2026-09-03-01.jpeg"],
+        date: "03 de Septiembre del 2026",
+        priority: 2,
+        links: [],
+        tags: []
+    },
     
 
 ];
